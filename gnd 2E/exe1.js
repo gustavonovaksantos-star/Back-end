55b7affafef49c8e7f6c2080cc9d5230bfad3935
gustavonovaksantos-star <gustavo.novak.santos@escola.pr.gov.br>
2026-06-19 12:26:30 -0300

    Add files via upload

---
+++ gnd 2E/exe1.js
@@ -0,0 +1,7 @@
+//Exercício 1: 
+let numero = 5;
+let texto = "10";
+console.log(numero + texto)
+
+//Transformar uma string em número 
+console.log(numero + Number(texto));
---
+++ gnd 2E/exe2.js
@@ -0,0 +1,18 @@
+//Exercício 2.
+//Declaração de variáveis 
+let a = 5;
+let b = 10;
+let c = (a + b);
+let soma = (a + b);
+let subtracao = (a - b);
+let multiplicacao = (a * b);
+let divisao = (a / b);
+
+//Resultados das contas:
+console.log("A:",a);
+console.log("B:",b);
+console.log("C:",c);
+console.log("A soma é:",soma);
+console.log("A subtração é:",subtracao);
+console.log("A multiplicação é:",multiplicacao);
+console.log("A divisão é:",divisao);
